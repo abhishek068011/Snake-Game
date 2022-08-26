2d91d17595dc4e26d70a8b6965801901392affbf
ABHISHEK MOHANTY <90187249+abhishek068011@users.noreply.github.com>
2022-08-26 19:35:12 +0530

    Add files via upload

---
+++ SNAKE GAME/src/Snake.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Snake extends JFrame {
+  Snake() {
+    add(new Board());
+    pack();
+    setLocationRelativeTo(null);
+    setResizable(false);
+    setTitle("The Snake Game");
+  }
+
+  public static void main(String[] args) {
+    new Snake().setVisible(true);
+  }
+} 
